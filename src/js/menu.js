4f1778f28781ae7ec625c4709e62c70de874c91b
Micah Gentzel <mgentzel@allebach.com>
2021-10-26 13:30:10 -0400

    initial review

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,9 +4,18 @@
         const primaryNavContainer = document.querySelector(`#${primaryNavButton.getAttribute('aria-controls')}`)
         primaryNavButton.addEventListener('click', function(e) {
             let expanded = primaryNavButton.getAttribute('aria-expanded')
-            console.log(expanded);
             primaryNavButton.setAttribute('aria-expanded', expanded == 'true' ? 'false' : 'true');
             primaryNavContainer.setAttribute('aria-expanded', expanded == 'true' ? 'false' : 'true');
         });
+
+        const menuItemsWithChildren = document.querySelectorAll('.menu-item-has-children a[href="#"]');
+        console.log(menuItemsWithChildren);
+        if(menuItemsWithChildren) {
+            for(var i of menuItemsWithChildren) {
+                i.addEventListener('click', function(e) {
+                    e.preventDefault();
+                });
+            }
+        }
     });
 })();
