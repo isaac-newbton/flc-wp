fbde04bb4ce47e3061a4b6bc3ec1ae502e050b40
Micah Gentzel <mgentzel@allebach.com>
2021-10-26 12:03:05 -0400

    initial review

---
+++ singular.php
@@ -0,0 +1,35 @@
+<?php
+
+get_header();
+
+$banner = get_field('banner_image');
+$hide_banner = get_field('_hide_banner');
+
+?>
+
+<?php if($banner && !$hide_banner): ?>
+<div class="page-banner-wrapper parallax-window" data-parallax="scroll" data-speed="0.75" data-image-src="<?=$banner?>">
+    <div class="content-container">
+        <div class="page-banner-container">
+            <div class="page-banner-content">
+                <h1><?=get_the_title()?></h1>
+            </div>
+        </div>
+    </div>
+</div>
+<?php endif; ?>
+
+<div class="content-sidebar-container">
+	<div class="content-container">
+		<?php
+			the_content();
+		?>
+	</div>
+
+	<?php get_sidebar('primary'); ?>
+
+</div>
+
+<?php
+
+get_footer();
